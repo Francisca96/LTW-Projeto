a97f2b6994940aa1362920f41a62180ccb2a6eaf
Francisca96 <franciscapauperio@gmail.com>
2016-12-10 18:17:51 +0000

    Css and html of main page

--- signin.php
+++ signin.php
@@ -1,22 +1,22 @@
 <?php
-  
+
 //incluir php com as funçoes precisas
-  
+
   $username=$_POST['username'];
   $password=$_POST['password'];
-  
+
   //$validPass = getPassword($username, $password);
-  
+
   if ($validPass == $password){
-	
+
     $userInfo = getUserInfo($username);
-	
+
     //ver valores necessários de ir buscar
-	
+
     //incluir php da pagina com login feito
   }
   else{
     include_once('login.php');
-    echo "User or Password incorrect";
+    echo "Username or Password incorrect";
   }
 ?>
--- signup.php
+++ signup.php
@@ -11,36 +11,36 @@ $passwordConfirmed=$_POST['passwordConfirm'];
 
 
 if($firstName && $lastName && $username && $password && $passwordConfirmed){
-    
+
 	if(strlen($password)<8){
         include_once('register_page.php');
         echo 'Password is too short. Please choose a new one.';
         return;
-    }
-	
-    if($password!==$passwordConfirmed){
+  }
+
+  if($password!==$passwordConfirmed){
         include_once('register_page.php');
         echo 'Password confirmation does not match.';
         return;
-    }
-	
+  }
+
 	//Funçao criar novo utilizador (verifica se é novo o username)
-    if(newUser($firstName,$lastName,$password,$email)==0){
-      $userInfo = getUserInfo($email);
-      $nomeCompleto = $userInfo['firstName'];
-      $nomeCompleto .= " ";
-      $nomeCompleto .= $userInfo['lastName'];
-      $_SESSION['email'] = $email;
-      $_SESSION['name'] = $nomeCompleto;
-      $_SESSION['id'] = $userInfo['id'];
-       
-	   //incluir php da pagina com login feito 
-	   
-    }
-    else {
-        include_once('register.php');
-        echo 'Invalid account. Username already exists.';
-        return;
-    }
+  if(newUser($firstName,$lastName,$password,$email)==0){
+    $userInfo = getUserInfo($email);
+    $nomeCompleto = $userInfo['firstName'];
+    $nomeCompleto .= " ";
+    $nomeCompleto .= $userInfo['lastName'];
+    $_SESSION['email'] = $email;
+    $_SESSION['name'] = $nomeCompleto;
+    $_SESSION['id'] = $userInfo['id'];
+
+	   //incluir php da pagina com login feito
+
+  }
+  else {
+    include_once('register.php');
+    echo 'Invalid account. Username already exists.';
+    return;
+  }
 }
 ?>
