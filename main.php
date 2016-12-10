165e89a53b199c49063e66b4063464b8e5296bf2
VascoRibeiro96 <vascovsc1996@gmail.com>
2016-12-10 19:29:17 +0000

    no message

--- main.php
+++ main.php
@@ -1,4 +1,4 @@
-
+<!DOCTYPE html>
 <html>
   <head>
     <title>Bloom</title>
@@ -6,17 +6,13 @@
     <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
-<<<<<<< HEAD:main.php
     <img src="images/bloom_main.png" alt="Site Logo">
-=======
-    <img src="bloom_main.png" alt="Site Logo">
     <div id="username">
 		  <input type="text" name="username" placeholder="username">
     </div>
     <div id="password">
       <input type="text" name="password" placeholder="password">
     </div>
->>>>>>> 4f1adc30f7a6418a4cd61541159097970428b95b:main.html
     <div id="buttons">
       <button id="signUpBtn" type="button">Sign up</button>
       <button id="loginBtn" type="submit">Login</button>
