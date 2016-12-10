a87d4c42fe85e1df22994b68076cb839b3e3bc6e
VascoRibeiro96 <vascovsc1996@gmail.com>
2016-12-10 19:21:52 +0000

    Organization

--- main.php
+++ main.php
@@ -1,11 +1,14 @@
-<!DOCTYPE html>
+
 <html>
   <head>
     <title>Bloom</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="css/main.css">
   </head>
   <body>
+<<<<<<< HEAD:main.php
+    <img src="images/bloom_main.png" alt="Site Logo">
+=======
     <img src="bloom_main.png" alt="Site Logo">
     <div id="username">
 		  <input type="text" name="username" placeholder="username">
@@ -13,6 +16,7 @@
     <div id="password">
       <input type="text" name="password" placeholder="password">
     </div>
+>>>>>>> 4f1adc30f7a6418a4cd61541159097970428b95b:main.html
     <div id="buttons">
       <button id="signUpBtn" type="button">Sign up</button>
       <button id="loginBtn" type="submit">Login</button>
