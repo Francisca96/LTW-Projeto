53bf10bf1a054e206be2666ad2125f5abe6fe35b
Francisca96 <franciscapauperio@gmail.com>
2016-12-11 19:30:15 +0000

    visitor page almost done
    the login page for owners and reviewers is similar

--- page/visitor.php
+++ page/visitor.php
@@ -6,22 +6,19 @@
     <link rel="stylesheet" href="../css/visitor.css">
 	</head>
 	<body>
-		<header>
-    	<div>
-				<input type="button" value="Login/Register" type="button" id="Login/Register"  onclick="window.location.href='../main.php';">
-    	</div>
-		</header>
-		<div id="middle">
-    	<img src="../images/logo.png"/>
-    	<div id="middle-elements">
-      	<div id="search-bar">
-      		<form action="" method="post">
-        		<input id="searchLocal" type="text" name="local_search" placeholder="Search by location..."/>
-          	<input id="searchRestaurant" type="text" name="restaurant_search" placeholder="Search by restaurant..."/>
-						<input type="submit" value="Search">
-        	</form>
-      	</div>
-    	</div>
+		<!-- <img id= "logo" src="../images/logo.png"/> -->
+    <div>
+			<input id="logReg" value="Login/Register" type="button"  onclick="window.location.href='../main.php';">
+    </div>
+		<img id= "logo_simples" src="../images/logo_simples.png"/>
+    <div id="middle-elements">
+  		<div id="search-bar">
+				<form action="search" method="post">
+      		<input id="searchLocal" type="text" name="local_search" placeholder="Search by location..."/>
+        	<input id="searchRestaurant" type="text" name="restaurant_search" placeholder="Search by restaurant..."/>
+					<input id="searchBtn" type="submit" value="Search">
+      	</form>
+			</div>
 		</div>
 	</body>
 </html>
